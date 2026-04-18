762ddc90b28c2de13ccc79939f81c0e16675aeaa
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-04-18 15:31:41 +0000

    "templates added"

--- PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/textutils/textutils/view.py
+++ PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/textutils/textutils/view.py
@@ -1,31 +1,26 @@
 #This file is created by me!!
 from django.http import HttpResponse
-
-#   ---------------   PERSONAL NAVIGATOR  ----------------
-# def index(request):
-#     return HttpResponse('''<h1>Hello Shivani!!</h1> <a href="https://www.youtube.com/">Youtube</a> ''')
-
-# def about(request):
-#     return HttpResponse("About hello Shivani")
+from django.shortcuts import render
 
 
 #-------------  PIPELINING -----------------
 def index(request):
-    return HttpResponse('''<h1>Home</h1> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/newlineremove">Remove NewLine</a><br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/removepunc">Remove Punctuation</a><br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/capitalizeFirst">Capitalize First</a><br></br><a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/spaceremove">Remove Space</a><br></br><a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/charcount">Count Characters</a><br></br>''')
+    return render(request,'index.html')
+    # return HttpResponse('''<h1>Home</h1> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/newlineremove">Remove NewLine</a><br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/removepunc">Remove Punctuation</a><br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/capitalizeFirst">Capitalize First</a><br></br><a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/spaceremove">Remove Space</a><br></br><a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/charcount">Count Characters</a><br></br>''')
 
 #---- Remove Punctuation -----
 def removepunc(request):
-    return HttpResponse('''remove punctuation !!<br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/">Back</a>''')
+    return HttpResponse('''remove punctuation !!<br></br> <a href="/">Back</a>''')
 
 def capfirst(request):
-    return HttpResponse('''Capitalize First.<br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/">Back</a>''')
+    return HttpResponse('''Capitalize First.<br></br> <a href="/">Back</a>''')
 
 def remline(request):
-    return HttpResponse('''Remove Newline <br></br>  <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/">Back</a>''')
+    return HttpResponse('''Remove Newline <br></br>  <a href="/">Back</a>''')
 
 def charcnt(request):
-    return HttpResponse('''Count Character <br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/">Back</a>''')
+    return HttpResponse('''Count Character <br></br> <a href="/">Back</a>''')
 
 def spcrem(request):
-    return HttpResponse('''Remove Space <br></br> <a href="https://congenial-disco-5gq5jx9jwpgrf7rqg-8000.app.github.dev/">Back</a>''')
+    return HttpResponse('''Remove Space <br></br> <a href="/">Back</a>''')
 
