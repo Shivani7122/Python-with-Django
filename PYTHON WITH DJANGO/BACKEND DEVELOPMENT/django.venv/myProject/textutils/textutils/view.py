ec2a39363f7dde19cf845a001a1bea53af9f3e86
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-04-18 13:04:45 +0000

    "Personal Navigator"

--- PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/textutils/textutils/view.py
+++ PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/textutils/textutils/view.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Hello Shivani!!</h1>")
+    return HttpResponse('''<h1>Hello Shivani!!</h1> <a href="https://www.youtube.com/">Youtube</a> ''')
 
 def about(request):
     return HttpResponse("About hello Shivani")
