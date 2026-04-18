66ba940646b5bf93f5c6e514895d2d83bf066c65
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-04-18 12:47:16 +0000

    "Views file added"

--- PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/textutils/textutils/view.py
+++ PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/textutils/textutils/view.py
@@ -1,1 +1,8 @@
 #This file is created by me!!
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Hello Shivani!!</h1>")
+
+def about(request):
+    return HttpResponse("About hello Shivani")
