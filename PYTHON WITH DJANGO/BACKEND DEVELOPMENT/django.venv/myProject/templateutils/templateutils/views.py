4ff2ae33f5ec2313605b91ebe376866399689c91
SHIVANI GUPTA <143402580+Shivani7122@users.noreply.github.com>
2026-04-23 14:10:59 +0000

    "Added new feature in templateutils"

--- PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/templateutils/templateutils/views.py
+++ PYTHON WITH DJANGO/BACKEND DEVELOPMENT/django.venv/myProject/templateutils/templateutils/views.py
@@ -11,14 +11,14 @@ def index(request):
 def analyze(request):
     #Get the text
     djtext = request.GET.get('text', 'default')
+    #check box values
     removepunc = request.GET.get('removepunc','off')
     fullcaps = request.GET.get('fullcaps','off')
     lowercase = request.GET.get('lowercase','off')
-    print(removepunc)
-    print(fullcaps)
-    print(lowercase)
-    print(djtext)
-    #Analyze the text
+    newlineremove = request.GET.get('newlineremove','off')
+    extraspaceremove = request.GET.get('extraspaceremove','off')
+    charcount = request.GET.get('charcount','off')
+    #Check which Checkbox is ON
     if removepunc=="on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed=""
@@ -40,6 +40,25 @@ def analyze(request):
         for char in djtext:
             analyzed = analyzed + char.lower()
         params = {'purpose':'Change to LowerCase', 'analyzed_text': analyzed}
+        return render(request,'analyze.html',params)
+    elif(newlineremove=="on"):
+        analyzed=""
+        for char in djtext:
+            if char != "/n":
+                analyzed = analyzed + char
+        
+        params = {'purpose': 'Removed New Line', 'analyzed_text': analyzed}
+        return render(request,'analyze.html',params)
+    elif(extraspaceremove=="on"):
+        analyzed=""
+        for index , char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index + 1]== " "):
+                analyzed = analyzed + char  
+        params = {'purpose': 'Remove Extra Space', 'analyzed_text': analyzed}
+        return render(request,'analyze.html',params)
+    elif(charcount=="on"):
+        analyzed=("No. of characters in the given string is "+str(len(djtext)))        
+        params = {'purpose': 'Count Characters', 'analyzed_text': analyzed}
         return render(request,'analyze.html',params)
     else:
         return HttpResponse("Error")
